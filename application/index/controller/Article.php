554c043f219a807e06d760e9fa58cc4c7d283062
isu5cn <573301558@qq.com>
2018-06-03 21:22:43 +0800

    1. 引入前台模版 2. 获取栏目列表 3. 获取二级栏目列表

---
+++ application/index/controller/Article.php
@@ -0,0 +1,41 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use app\common\model\Category as Cate;
+use app\common\model\Article as Art;
+
+class Article extends Controller{
+
+	private $cate = null;
+	private $art = null;
+    public function _initialize(){
+        parent::_initialize();
+        $this->cate = new Cate();
+        $this->art = new Art();
+    }
+
+    //列表页
+    public function list(){
+    	$id = input('param.pid');
+    	//dump($pid);
+       $data =  $this->art->field('id,bigtitle,author')->where('cid',$id)->select();
+       //需要分页
+       //dump($data);
+    	$this->assign('data',$data);
+        return view();
+
+    }
+
+    //内容页
+    public function item(){
+    	$id = input('param.pid');
+    	//dump($pid);
+       $data =  $this->art->where('cid',$id)->select();
+
+       //dump($data);
+    	$this->assign('data',$data);
+        return view();
+    }
+
+
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,15 +2,32 @@
 namespace app\index\controller;
 
 use think\Controller;
+use app\common\model\Category as Cate;
 
 class Index extends Controller
 {
+	private $cate = null;
+    public function _initialize(){
+        parent::_initialize();
+        $this->cate = new Cate();
+    }
+    //首页
     public function index()
     {
 
-    	$data = db('category')->order('sort desc, id desc')->select();
-    	
-    	getTree();
+    	$data = $this->cate->where('pid',0)->select();
+    	//dump($data);
+    	$this->assign('data',$data);
         return view();
     }
+
+    //栏目列表
+    public function cate(){
+    	$id = input('param.pid');
+    	$data = $this->cate->where('pid',$id)->select();
+    	//dump($data);
+    	$this->assign('data',$data);
+    	return view();
+    }
+
 }
